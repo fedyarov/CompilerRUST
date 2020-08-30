b7d80668b52621af94e8db562615f35a40681833
Max Fedyarov <collofdu2@mail.ru>
2020-08-30 17:58:12 +0300

    Add class Token

---
+++ Token.cpp
@@ -0,0 +1,44 @@
+#include "Token.h"
+
+Token::Token(const string& lexeme)
+{
+	this->lexeme = lexeme;
+
+	this->type = get_type();
+}
+
+token_type Token::get_type()
+{
+	if (is_integer(lexeme)) {
+		return token_type::NUMBER;
+	}
+	if (lexeme == "+") {
+		return token_type::PLUS;
+	}
+	if (lexeme == "-") {
+		return token_type::MINUS;
+	}
+	if (lexeme == "*") {
+		return token_type::STAR;
+	}
+	if (lexeme == "/") {
+		return token_type::SLASH;
+	}
+	if (lexeme == "(") {
+		return token_type::RPAR;
+	}
+	if (lexeme == ")") {
+		return token_type::RPAR;
+	}
+	return token_type::UNDEFINED;
+}
+
+bool Token::is_integer(const string& lexeme)
+{
+	for (const auto& symbol : lexeme) {
+		if (symbol < '0' || symbol > '9') {
+			return false;
+		}
+	}
+	return true;
+}
