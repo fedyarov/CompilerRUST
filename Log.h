032a7f13af4baf4fe9107aacb40572201f0a3b54
Max Fedyarov <collofdu2@mail.ru>
2020-08-31 15:03:22 +0300

    Add the log file

---
+++ Log.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void create_new_log();
+
+void cout_log(string message);
+void cout_log(int num);
+
+void endl_log();
