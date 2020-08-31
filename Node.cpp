d51cecd9369aad9ee2bc32c12875b1285e8ef442
Max Fedyarov <collofdu2@mail.ru>
2020-08-31 23:09:56 +0300

    Improved write to log

--- Node.cpp
+++ Node.cpp
@@ -1,10 +1,20 @@
 #include "Node.h"
 
-Node::Node(node_type type, const string value = "", Node* operand1 = nullptr,
-	Node* operand2 = nullptr, Node* operand3 = nullptr, Node* operand4 = nullptr)
+Node::Node(node_type type, const string value, Node* operand1,
+	Node* operand2, Node* operand3, Node* operand4)
 {
 	this->type = type;
 	this->value = value;
+	this->operand1 = operand1;
+	this->operand2 = operand2;
+	this->operand3 = operand3;
+	this->operand4 = operand4;
+}
+
+Node::Node(node_type type, Node* operand1, Node* operand2,
+	Node* operand3, Node* operand4)
+{
+	this->type = type;
 	this->operand1 = operand1;
 	this->operand2 = operand2;
 	this->operand3 = operand3;
