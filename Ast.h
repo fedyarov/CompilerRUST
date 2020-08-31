f8bc3f5c72354747351348113aadb6f37d9c9e31
Max Fedyarov <collofdu2@mail.ru>
2020-08-31 15:35:12 +0300

    Add the ast class

---
+++ Ast.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Node.h"
+#include "Log.h"
+
+class Ast
+{
+public:
+	Node* tree;
+public:
+	Ast();
+	~Ast();
+
+	void descent_recursive(Node* current_node);
+	void print_recursive(Node* current_node, size_t level);
+	void print();
+};
+
