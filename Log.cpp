053d7a402d84b3e75bed8e73fe96e0858da6306b
Max Fedyarov <collofdu2@mail.ru>
2020-08-31 15:03:35 +0300

    Add the log file

---
+++ Log.cpp
@@ -0,0 +1,41 @@
+#include "Log.h"
+
+void create_new_log()
+{
+	ofstream log_file("log.txt");
+	if (log_file.is_open()) {
+		log_file.close();
+
+		cout << "INFO: New log file created successfully" << endl;
+	}
+}
+
+void cout_log(string message)
+{
+	ofstream log_file("log.txt", ios::app);
+	if (log_file.is_open()) {
+		log_file << message;
+
+		log_file.close();
+	}
+}
+
+void cout_log(int num)
+{
+	ofstream log_file("log.txt", ios::app);
+	if (log_file.is_open()) {
+		log_file << num;
+
+		log_file.close();
+	}
+}
+
+void endl_log()
+{
+	ofstream log_file("log.txt", ios::app);
+	if (log_file.is_open()) {
+		log_file << endl;
+
+		log_file.close();
+	}
+}
