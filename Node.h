a6a5ee3a4d58b16446f27c72a4ba591349b20518
Max Fedyarov <collofdu2@mail.ru>
2020-08-31 15:35:55 +0300

    Add the NODE classs

---
+++ Node.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+enum node_type {
+	PROG, // HEAD OF TREE
+	EXPR, // EXPRESSION
+
+	CONSTANT, 
+	ADD, // +
+	SUB, // -
+	MUL, // *
+	DIV, // /
+};
+
+class Node
+{
+public:
+	string value;
+	node_type type;
+public:
+
+	Node(node_type type, const string value ="", Node* operand1 = nullptr,
+		Node* operand2 = nullptr, Node* operand3 = nullptr, Node* operand4 = nullptr);
+
+	Node* operand1;
+	Node* operand2;
+	Node* operand3;
+	Node* operand4;
+};
+
